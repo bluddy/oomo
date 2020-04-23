a42f4103a19a3d146d9345c31d141f7d5f136855
Yotam Barnoy <yotambarnoy@gmail.com>
2020-04-23 02:55:44 -0400

    num: more

--- num.ml
+++ num.ml
@@ -104,7 +104,7 @@ let num_ng_tech = [|
 |]
 
 (* per race, per trait1 *)
-let num_tbl_trait1[RACE_NUM][TRAIT1_TBL_NUM] = [|
+let num_tbl_trait1 = [|
     [| 4; 4; 4; 4; 4; 4; 4; 5; 5; 3 |];
     [| 1; 1; 1; 1; 1; 1; 1; 2; 2; 0 |];
     [| 0; 0; 0; 0; 0; 0; 0; 2; 2; 3 |];
@@ -115,10 +115,10 @@ let num_tbl_trait1[RACE_NUM][TRAIT1_TBL_NUM] = [|
     [| 2; 2; 2; 2; 2; 2; 2; 3; 3; 1 |];
     [| 3; 3; 3; 3; 3; 3; 3; 2; 2; 0 |];
     [| 2; 2; 2; 2; 2; 2; 2; 1; 1; 0 |]
-|];
+|]
 
 (* per race; per trait2 *)
-let num_tbl_trait2[RACE_NUM][TRAIT2_TBL_NUM] = [|
+let num_tbl_trait2 = [|
     [| 0; 0; 0; 0; 1; 2; 3; 4; 4; 0; 3; 5 |];
     [| 0; 1; 1; 1; 1; 3; 2; 2; 3; 3; 4; 5 |];
     [| 0; 1; 2; 2; 2; 2; 3; 3; 3; 4; 4; 5 |];
@@ -129,6 +129,6 @@ let num_tbl_trait2[RACE_NUM][TRAIT2_TBL_NUM] = [|
     [| 0; 5; 5; 5; 5; 2; 2; 3; 3; 3; 4; 5 |];
     [| 0; 1; 2; 2; 3; 3; 3; 4; 4; 4; 4; 5 |];
     [| 0; 0; 3; 3; 1; 2; 0; 3; 3; 4; 4; 5 |]
-|];
+|]
 
-let num_tbl_tech_autoadj[4] = [| 0; 25; 50; 75 |];
+let num_tbl_tech_autoadj = [| 0; 25; 50; 75 |]
